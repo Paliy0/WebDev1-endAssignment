f58ae4c2e0b97565720da487bc44178cbdcdaf73
Pablo Gulias <pabloguliasprats@gmail.com>
2026-01-16 01:54:47 +0100

    add dynamic port env

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
+# Railway provides PORT environment variable
+PORT=${PORT:-80}
+
+# Replace port in nginx config
+sed -i "s/listen 80/listen $PORT/" /etc/nginx/nginx.conf
+
 # Start PHP-FPM in background
 php-fpm -D
 
