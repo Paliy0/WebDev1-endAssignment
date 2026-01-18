f3bb93ea3436cfb526bca848edad668d47b8d623
Pablo Gulias <pabloguliasprats@gmail.com>
2026-01-18 12:43:38 +0100

    add duplicate route for POST method

--- app/public/routes/admin.php
+++ app/public/routes/admin.php
@@ -15,7 +15,12 @@ Route::add('/admin/users', function () {
 Route::add('/admin/users/edit/([0-9]+)', function ($userId) {
     $adminController = new AdminController();
     $adminController->editUser($userId);
-});
+}, 'get');
+
+Route::add('/admin/users/edit/([0-9]+)', function ($userId) {
+    $adminController = new AdminController();
+    $adminController->editUser($userId);
+}, 'post');
 
 Route::add('/admin/users/delete/([0-9]+)', function ($userId) {
     $adminController = new AdminController();
@@ -35,7 +40,12 @@ Route::add('/admin/shops', function () {
 Route::add('/admin/shops/edit/([0-9]+)', function ($shopId) {
     $adminController = new AdminController();
     $adminController->editShop($shopId);
-});
+}, 'get');
+
+Route::add('/admin/shops/edit/([0-9]+)', function ($shopId) {
+    $adminController = new AdminController();
+    $adminController->editShop($shopId);
+}, 'post');
 
 Route::add('/admin/shops/delete/([0-9]+)', function ($shopId) {
     $adminController = new AdminController();
